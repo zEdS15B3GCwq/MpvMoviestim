a0ddf2bfb3de713faad271a58e6135f570e7fdcc
Tamas Fehervari <58502181+zEdS15B3GCwq@users.noreply.github.com>
2026-09-18 10:42:29 +0900

    fix: remove shadow window from pyglet's window list to prevent crash on window move

--- src/mpvmoviestim/utils.py
+++ src/mpvmoviestim/utils.py
@@ -685,7 +685,7 @@ def create_shadow_window(main_window: Any) -> Any:
     shadow_window = pyglet.window.Window(width=100, height=100, visible=False)
     pyglet.app.windows.remove(
         shadow_window
-    )  # keep it out of pyglet's global event/idle loop
+    )  # keep it out of pyglet's global event/idle loop to prevent crashin on window move
 
     # is this necessary to hand-off context?
     # shadow_window.switch_to()  # redundant, already in Window.__init__()
